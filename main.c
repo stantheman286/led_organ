4aac2dc0f5d26d693def4e1f6e422e3bc5bc25fa
Matt Staniszewski <matt.staniszewski@gmail.com>
2014-03-31 23:39:21 -0700

    Created MPLAB X project and empty main source file

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Matt Staniszewski
+ *
+ * Created on March 31, 2014, 11:38 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
